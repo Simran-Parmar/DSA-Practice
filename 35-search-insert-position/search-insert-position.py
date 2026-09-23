1b6dd732fc9a27b0f10d7fe1965973b871213c46
Simran-Parmar <121112012+Simran-Parmar@users.noreply.github.com>
2026-09-23 20:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (31.57%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,59 +1,16 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        high = len(nums)-1
+        indx = len(nums)
         low = 0
-        lowest = float('inf')
-        lowerbound = len(nums) 
+        high = len(nums)-1
         while low<=high:
             mid = (low+high)//2
             if target<=nums[mid]:
-                if nums[mid]<lowest:
-                    lowerbound = mid
                 high = mid-1
+                indx = mid
             else:
                 low = mid+1
-        return lowerbound
-            
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # indx = len(nums)
-        # low = 0
-        # high = len(nums)-1
-        # while low<=high:
-        #     mid = (low+high)//2
-        #     if target<=nums[mid]:
-        #         high = mid-1
-        #         indx = mid
-        #     else:
-        #         low = mid+1
-        # return indx
+        return indx
                 
 
         
