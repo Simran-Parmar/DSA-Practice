fa5aafccd1acfd98d698467d9cea88e39c95bfb1
Simran-Parmar <121112012+Simran-Parmar@users.noreply.github.com>
2026-09-23 17:30:34 +0530

    Time: 647 ms (10.72%) | Memory: 12.5 MB (29.48%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def fourSum(self, nums, target):
         n = len(nums)
-        result = []
         nums.sort()
-        for i in range(0,n-2):
-            if i !=0 and nums[i]==nums[i-1]:
+        result = []
+        for i in range(0,n):
+            if i!=0 and nums[i-1]==nums[i]:
                 continue
             for j in range(i+1,n-1):
-                if j != i+1 and nums[j]==nums[j-1]:
+                if j!=i+1 and nums[j-1]==nums[j]:
                     continue
                 k = j+1
                 l = n-1
@@ -21,11 +21,63 @@ class Solution(object):
                         result.append(temp)
                         k = k+1
                         l = l-1
-                        while nums[k]==nums[k-1] and k<l:
+                        while k<l and nums[k-1] == nums[k]:
                             k = k+1
-                        while nums[l]==nums[l+1] and k<l:
+                        while k<l  and nums[l+1]==nums[l]:
                             l = l-1
         return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(nums)
+        # result = []
+        # nums.sort()
+        # for i in range(0,n-2):
+        #     if i !=0 and nums[i]==nums[i-1]:
+        #         continue
+        #     for j in range(i+1,n-1):
+        #         if j != i+1 and nums[j]==nums[j-1]:
+        #             continue
+        #         k = j+1
+        #         l = n-1
+        #         while k<l:
+        #             if nums[i]+nums[j]+nums[k]+nums[l]<target:
+        #                 k = k+1
+        #             elif nums[i]+nums[j]+nums[k]+nums[l]>target:
+        #                 l = l-1
+        #             else:
+        #                 temp = [nums[i],nums[j],nums[k],nums[l]]
+        #                 result.append(temp)
+        #                 k = k+1
+        #                 l = l-1
+        #                 while nums[k]==nums[k-1] and k<l:
+        #                     k = k+1
+        #                 while nums[l]==nums[l+1] and k<l:
+        #                     l = l-1
+        # return result
 
 
 
