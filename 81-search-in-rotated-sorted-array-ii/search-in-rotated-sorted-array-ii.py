f6e3eec1ea652dc1b8abfefbf7ec8ceb4fa7ec2d
Simran-Parmar <121112012+Simran-Parmar@users.noreply.github.com>
2026-09-24 14:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (6.43%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,26 +1,71 @@
 class Solution(object):
     def search(self, nums, target):
+        n = len(nums)
         low = 0
-        high = len(nums)-1
+        high = n-1
         while low<=high:
             mid = (low+high)//2
             if nums[mid]==target:
                 return True
-            elif nums[mid]==nums[low]==nums[high]:
-                low = low+1
-                high = high-1
-            else:
-                if nums[mid+1]<=nums[high]:
-                    if nums[mid+1]<=target<=nums[high]:
-                        low = mid+1
-                    else:
-                        high = mid-1
+            while low<high and nums[low]==nums[mid]==nums[high]:
+                low = low + 1
+                high = high - 1
+            if nums[mid]<=nums[high]:
+                if nums[mid]<=target<=nums[high]:
+                    low = mid+1
                 else:
-                    if nums[low]<=target<=nums[mid-1]:
-                        high = mid-1
-                    else:
-                        low = mid+1
+                    high = mid-1
+            else:
+                if nums[low]<=target<=nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # low = 0
+        # high = len(nums)-1
+        # while low<=high:
+        #     mid = (low+high)//2
+        #     if nums[mid]==target:
+        #         return True
+        #     elif nums[mid]==nums[low]==nums[high]:
+        #         low = low+1
+        #         high = high-1
+        #     else:
+        #         if nums[mid+1]<=nums[high]:
+        #             if nums[mid+1]<=target<=nums[high]:
+        #                 low = mid+1
+        #             else:
+        #                 high = mid-1
+        #         else:
+        #             if nums[low]<=target<=nums[mid-1]:
+        #                 high = mid-1
+        #             else:
+        #                 low = mid+1
+        # return False
 
 
         
